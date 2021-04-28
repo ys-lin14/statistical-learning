b21bbb3c5955890b3ef88fe355a86a0992c90e00
Derek Lin <ys.lin113@gmail.com>
2021-04-27 18:11:34 -0700

    use updated evaluate_classifier function

--- 4_classification/ch4_metrics.py
+++ 4_classification/ch4_metrics.py
@@ -1,26 +1,28 @@
-from sklearn.metrics import (
-    accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
-)
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
 
 def get_metrics(X, y, clf, threshold=0.5):
     # Get metrics for binary classifier
     y_probabilities = clf.predict_proba(X)
     y_pred = (y_probabilities[:, 1] > threshold).astype(int)
-    # TO DO: accuracy for null classifier
+    
+    unique_results = np.unique(y, return_counts=True)
+    most_common_label = unique_results[0][unique_results[1].argmax()]
+    y_null = np.full_like(y, most_common_label)
 
     accuracy = accuracy_score(y, y_pred)
+    null_accuracy = accuracy_score(y, y_null)
     confusion = confusion_matrix(y, y_pred)
     f1 = f1_score(y, y_pred).round(3)
     precision = precision_score(y, y_pred).round(3)
     recall = recall_score(y, y_pred).round(3)
 
-    return accuracy, confusion, f1, precision, recall
+    return accuracy, null_accuracy, confusion, f1, precision, recall
 
-def display_metrics(accuracy, confusion, f1, precision, recall):
+def display_metrics(accuracy, null_accuracy, confusion, f1, precision, recall):
     # Print metrics for binary classifier
     print('Accuracy:', accuracy)
-    # TO DO: metrics for null classifier
-    # print('Null Accuracy:{0}\n'.format(null_accuracy))
+    print('Null Accuracy:{0}\n'.format(null_accuracy))
     
     print('\nConfusion Matrix:\n{0}\n'.format(confusion))
     print('F1 Score:', f1)
